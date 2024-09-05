4866458fd0b0af85f429c1de529f2bad18849c1e
Manish Gupta <manishguptacpn@gmail.com>
2024-09-06 01:43:31 +0530

    Added Shadcn ✨

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
 const Home = () => {
     return (
         <>
-            Home
+            <div className="flex p-8 w-full h-screen justify-center items-center">
+                <Button>Home</Button>
+            </div>
         </>
     );
 }
